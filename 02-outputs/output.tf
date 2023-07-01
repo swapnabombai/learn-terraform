0f1a52dacca33fdc21b1dc7f0a3f05885341e0a3
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-07-01 20:27:34 +0530

    terraform basics

---
+++ 02-outputs/output.tf
@@ -0,0 +1,10 @@
+output "sample" {
+  value = "Hello World"
+}
+## any  string in terraform need to be quoted with double quotes only
+##single quotes are not supported
+
+output "sample1" {
+  value = "1000"
+}
+# numbers and booleans (trie,false)
