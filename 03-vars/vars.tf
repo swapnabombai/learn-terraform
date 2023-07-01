261a1a9613b052f2f8d76a26a174ab4b71ee991b
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-07-01 20:43:00 +0530

    terraform basics.variables

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -5,10 +5,10 @@ variable "sample1" {
   default = "hello world"
 }
 output "sample" {
-  value = "var.sample"
+  value = var.sample
 }
 output "sample1" {
-  value = "var.sample1"
+  value = var.sample1
 }
 ##sometimes if variables/any reference with the combination of some other string then we have to access those in ${}
 output "sample-ext" {
