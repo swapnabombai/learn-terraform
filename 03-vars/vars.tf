e336870fba776cfaab79569de1e7cc6c442a2cd5
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-07-01 20:39:20 +0530

    terraform basics.variables

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -10,3 +10,7 @@ output "sample" {
 output "sample1" {
   value = "var.sample1"
 }
+##sometimes if variables/any reference with the combination of some other string then we have to access those in ${}
+output "sample-ext" {
+  value = "value of sample-${var.sample}"
+}
