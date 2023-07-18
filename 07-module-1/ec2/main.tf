e61c397f8aa81c354f0c7cf7bc34b68479e4d143
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-07-18 12:06:01 +0530

    jenkins

--- 07-module-1/ec2/main.tf
+++ 07-module-1/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = var.name
