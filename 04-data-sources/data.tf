96eacf0b53fa1c656a9b46da9dcd8a4e9a86efa4
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-07-18 11:33:43 +0530

    jenkins

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -10,20 +10,3 @@ data "aws_ec2_spot_price" "example" {
 output "price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
-data "aws_security_group" "selected" {
-  name= "allow-all"
-}
-output "sgid" {
-  value = data.aws_security_group.selected.id
-}
-data "aws_ami" "example" {
-  owners=["973714476881"]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-}
-output "ami" {
-  value = data.aws_ami.example.id
-}
-provider "aws" {
-  region = "us-east-1"
-}
